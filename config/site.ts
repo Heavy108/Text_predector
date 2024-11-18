9fe5885a75ba43b112810c394d4c231a8d4dacf4
Heavy108 <Heavy1089@gmail.com>
2024-11-18 23:06:31 +0530

    change the github address

--- config/site.ts
+++ config/site.ts
@@ -60,7 +60,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
+    github: "https://github.com/Heavy108/Text_predector",
     twitter: "https://twitter.com/getnextui",
     docs: "https://nextui.org",
     discord: "https://discord.gg/9b6yyZKmH4",
