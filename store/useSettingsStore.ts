4040b8fefbee8f0ac168d16579a07dfd27ca111f
Heavy108 <Heavy1089@gmail.com>
2024-11-20 23:22:20 +0530

    Suggestion set to false

--- store/useSettingsStore.ts
+++ store/useSettingsStore.ts
@@ -5,8 +5,9 @@ interface SettingsState {
   toggleNotifications: () => void;
 }
 
+// eslint-disable-next-line prettier/prettier
 export const useSettingsStore = create<SettingsState>((set) => ({
-  notifications: true,
+  notifications: false,
   toggleNotifications: () =>
     set((state) => ({ notifications: !state.notifications })),
 }));
